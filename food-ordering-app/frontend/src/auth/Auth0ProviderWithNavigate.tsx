43424451123d02c4cb841de7e8ee4f8d3e566ae4
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-07-28 22:21:45 +0530

    Auth0 provider setup complete

--- food-ordering-app/frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ food-ordering-app/frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,8 +1,9 @@
-import { Auth0Provider } from "@auth0/auth0-react";
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
     children: React.ReactNode;
 }
+
 function Auth0ProviderWithNavigate( { children }: Props) {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -12,6 +13,13 @@ function Auth0ProviderWithNavigate( { children }: Props) {
         throw new Error("Unable to intialise auth");
     }
 
+    const onRedirectCallback = (appState?: AppState, user?: User) => {
+        console.log("USER", user);
+                
+
+    }
+
+
   return (
     <Auth0Provider 
     domain={domain} 
@@ -19,6 +27,7 @@ function Auth0ProviderWithNavigate( { children }: Props) {
     authorizationParams={{
         redirect_uri: redirectUri
     }}
+    onRedirectCallback={onRedirectCallback}
     >
         {children}
     </Auth0Provider>
