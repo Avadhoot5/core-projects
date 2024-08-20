ce1aee021d27fc554699d023a6dcfa5ab099f7d8
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-08-20 18:51:49 +0530

    login functionality added

--- buddy-workout/server/controller/userController.js
+++ buddy-workout/server/controller/userController.js
@@ -8,12 +8,21 @@ const createToken = (_id) => {
     return jwt.sign({_id}, process.env.SECRET, { expiresIn: '1d' });
 }
 
-const loginUser = (req, res) => {
-    res.json({mssg: 'User logged in' });
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.status(200).json({email, token});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
 }
 
 const signupUser = async (req, res) => {
     const { email, password } = req.body;
+    
     try {
         const user = await User.signup(email, password);
 
@@ -21,7 +30,7 @@ const signupUser = async (req, res) => {
         const token = createToken(user._id);
         res.status(200).json({email, token});    
     } catch (error) {
-        res.json({error: error.message});
+        res.status(400).json({error: error.message});
     }
 }
 
