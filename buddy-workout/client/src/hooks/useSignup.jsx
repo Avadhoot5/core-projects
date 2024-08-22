9ec638f3329a0b2a8672ebb66f11d9c9ba35fee8
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-08-22 11:20:04 +0530

    useSignup hook for connected to backend

---
+++ buddy-workout/client/src/hooks/useSignup.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import { BASE_URL } from '../App';
+
+
+export const useSignup = () => {
+
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const { dispatch } = useAuthContext();
+
+    const signup = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch(`${BASE_URL}api/user/signup`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({email, password})
+        })
+        const data = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(data.error);
+        }
+
+        if (response.ok) {
+            setIsLoading(false);
+
+            // save the token in browser
+            localStorage.setItem('user', data);
+            
+            // set the auth state
+            dispatch({type: 'LOGIN', payload: data});
+        }
+    }
+    return { signup, error, isLoading }
+}
+
