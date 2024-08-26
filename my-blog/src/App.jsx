75123f787a67024ac6580f6c729dba723decd659
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-08-26 22:44:19 +0530

    multiple elements in the page

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -2,9 +2,18 @@ import './App.css'
 
 function App() {
 
+  const title = 'Welcome to the new Blog';
+  const number = 23;
+  const arr = [23,1,1];
+
+
   return (
     <>
-      <div>My Blog</div>
+      <div>
+        <h1>{title}</h1>
+        <p>{number}</p>
+        <p>{arr}</p>
+      </div>
     </>
   )
 }
