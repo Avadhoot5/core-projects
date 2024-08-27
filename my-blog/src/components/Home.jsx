b0c063d0364ccfd0db6153dce4535d0788f5acc2
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-08-27 11:09:32 +0530

    handling click events: button

--- my-blog/src/components/Home.jsx
+++ my-blog/src/components/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("hello there", e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log(`Hello ${name}, ${e.target}`);
+    }
+    
   return (
     <div className='home'>
         <h2>Homepage</h2>
+        <div>
+            {/* we cannot call functinons as, fn(), since it gets call automatically
+            and when we want to pass arguments to the fn we can use anonymous arrow fn */}
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('test',e)}>Click me again</button>
+        </div>
     </div>
   )
 }
