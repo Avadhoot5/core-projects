e158cb40d7215cf419c93f6e7660f982c8aa5726
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-08-27 11:01:06 +0530

    added css styles

--- my-blog/src/components/Navbar.jsx
+++ my-blog/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
         <h1>My BLOG</h1>
-        {/* <Link to='/home'>New Blog</Link> */}
+       <div className="links">
+        <a href="#">Home</a>
+        <a href="#">New Blog</a>
+       </div>
 
     </div>
   )
