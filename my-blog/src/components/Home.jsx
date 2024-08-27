78237ebec1022ba46194880c879a0846892e1257
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-08-27 13:39:27 +0530

    rendering blog data in front end

--- my-blog/src/components/Home.jsx
+++ my-blog/src/components/Home.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log("hello there", e);
-    }
+    const [blogs, setBlogs] = useState([
+        {title: "new blog", body: "lorem ipsum", author: "test", id: 1},
+        {title: "blog is new", body: "lorem ipsum", author: "authr2", id: 2},
+        {title: "hell from blog", body: "lorem ipsum", author: "new author", id: 3},
+    ])
 
-    const handleClickAgain = (name, e) => {
-        console.log(`Hello ${name}, ${e.target}`);
-    }
-    
   return (
     <div className='home'>
         <h2>Homepage</h2>
         <div>
-            {/* we cannot call functinons as, fn(), since it gets call automatically
-            and when we want to pass arguments to the fn we can use anonymous arrow fn */}
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('test',e)}>Click me again</button>
+            {blogs.map((blog) => (
+                <div className='blog-preview' key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     </div>
   )
 }
+
+{/* we cannot call functinons as, fn(), since it gets call automatically
+and when we want to pass arguments to the fn we can use anonymous arrow fn */}
 
 export default Home
