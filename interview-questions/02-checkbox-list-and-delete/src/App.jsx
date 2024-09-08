4370883142834adc243e917ab5d6dd7aa1a75791
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-09-08 23:54:22 +0530

    checkbox, delete logic added. Need to fix - checkbox state array

---
+++ interview-questions/02-checkbox-list-and-delete/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import './App.css'
+
+
+function App() {
+
+  let arr = ['Play cricket', 'Play video game', 'read book', 'complete react', 'get a job'];
+  let checkState = Array(arr.length).fill(false);
+
+  const [todo, setTodo] = useState(arr);
+  const [checkBox, setCheckBox] = useState(checkState);
+
+  const handleCross = (index) => {
+    let filterArr = todo.filter((value, i) => i !== index);
+    setTodo(filterArr);
+    setCheckBox(prev => prev.splice(index, 1));
+  }
+  console.log(checkBox);
+
+  return (
+    <>
+      <>
+         {/*displaying the array items  */}
+         <h2>Todo List</h2>
+         {todo.map((value, index) => {
+          return (
+            <div key={index}
+            style={{maxWidth:'15%', display:'flex', gap: '10px', justifyContent: 'space-between'}}>
+              <span>
+                <input type="checkbox" name={index}
+                onChange={() => {
+                  setCheckBox((prev) => {
+                    const newCheckBox = [...prev];
+                    newCheckBox[index] =  !newCheckBox[index];
+                    return newCheckBox;
+                  });
+                }}/>
+              </span>
+              {value && (
+                <div>
+                  {value}
+                  {checkBox[index] && <CrossMark handleCross={handleCross} index={index}/>}
+                </div>
+              )}
+            </div>
+          )
+         })}
+      </>
+    </>
+  )
+}
+
+function CrossMark({handleCross, index}) {
+  return (<>
+    <button
+      onClick={() => {
+        handleCross(index)
+      }}> [X]
+    </button>
+  </>)
+}
+
+export default App
