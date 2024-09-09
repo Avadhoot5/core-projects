f08c722c366ed20aa57682761beef7689efed925
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-09-09 17:46:13 +0530

    completed the static page

--- practice-project/02-contactUs-page/src/components/HeroSection/HeroSection.tsx
+++ practice-project/02-contactUs-page/src/components/HeroSection/HeroSection.tsx
@@ -1,28 +1,30 @@
 import './HeroSection.css';
+import Button from '../Button/Button';
 
 const HeroSection = () => {
   return (
-    <div>
-        <div className="mainContent">
-            <div className="heroContent">
+    <div className='heroMain'>
+        <div>
+            <div className="mainContent">
                 <div className="heroHeading">
                         YOUR FEET 
                         DESERVE
                         THE BEST
                 </div>
                 <div className="heroDescription">
-
+                    YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
                 </div>
                 <div className="shopButtons">
-
+                    <Button name='Shop Now' color='red' textColor='white'/>
+                    <Button name='Category' color='white' textColor='black'/>
                 </div>
                 <div className="amazonFlipkart">
-
+                    <img src="../public/logos/shop.png" alt="amazon-flipkart-Logo" />
                 </div>
             </div>
         </div>
         <div className="heroImage">
-
+            <img src="../public/logos/shoe_image.png" alt="shoeImage" />
         </div>
     </div>
   )
